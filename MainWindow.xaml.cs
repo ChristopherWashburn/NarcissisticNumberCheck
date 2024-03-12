723fd720f2df30ef2df8d1f7bbaa611f8ddc5702
cfwas <cfwashbu@gmail.com>
2024-03-11 22:14:56 -0400

    Finished function to calculate narcassistic number

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,7 +23,8 @@ namespace NarcissisticNumberCheck
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            double dblSubTotal = 0;
+            double dblNarcNum = 0;
+            double dblCharNum = 0;
 
             //get input number from txtNumber
             string strInputNum = txtNumber.Text;
@@ -36,13 +37,15 @@ namespace NarcissisticNumberCheck
 
             //raise each digit to the power of the total number of digits
             //of input number and add to total 
-            foreach (char c in charDigits)
+            foreach(var c in charDigits)
             {
-                dblSubTotal += Math.Pow(intNumDigits, c);
+                //dblNarcNum = Math.Pow(c, intNumDigits);
+                dblCharNum = Char.GetNumericValue(c);
+                dblNarcNum += Math.Pow(dblCharNum, intNumDigits);
             }
 
             //check if it is equal to original number
-            if (dblSubTotal == Double.Parse(strInputNum))
+            if (dblNarcNum == Int32.Parse(strInputNum))
             {
                 lblResult.Content = "This is a Narcissistic Number!";
             }
